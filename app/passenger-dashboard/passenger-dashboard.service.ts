dab1d449d50e253bcc1254a6c8c0bcc626eac8cc
johnmrox <johnmrox@gmail.com>
2017-11-26 11:02:38 -0600

    observable.catch error handling section

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -3,6 +3,8 @@ import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 import { Passenger } from './models/passenger.interface';
 
@@ -12,7 +14,10 @@ const PASSENGER_API: string = '/api/passengers';
 export class PassengerDashboardService {
 	constructor(private http: Http) {}
 	getPassengers(): Observable<Passenger[]> {
-		return this.http.get(PASSENGER_API).map((response: Response) => response.json());
+		return this.http
+			.get(PASSENGER_API)
+			.map((response: Response) => response.json())
+			.catch((error: any) => Observable.throw(error.json()));
 	}
 	updatePassenger(passenger: Passenger): Observable<Passenger> {
 		let headers = new Headers({
@@ -23,9 +28,13 @@ export class PassengerDashboardService {
 		});
 		return this.http
 			.put(`${PASSENGER_API}/${passenger.id}`, passenger, options)
-			.map((response: Response) => response.json());
+			.map((response: Response) => response.json())
+			.catch((error: any) => Observable.throw(error.json()));
 	}
 	removePassenger(passenger: Passenger): Observable<Passenger> {
-		return this.http.delete(`${PASSENGER_API}/${passenger.id}`).map((response: Response) => response.json());
+		return this.http
+			.delete(`${PASSENGER_API}/${passenger.id}`)
+			.map((response: Response) => response.json())
+			.catch((error: any) => Observable.throw(error.json()));
 	}
 }

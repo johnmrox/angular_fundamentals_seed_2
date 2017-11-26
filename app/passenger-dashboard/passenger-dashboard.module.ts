a42135663d73c19199d1c7c2cf894eb95f3e5287
johnmrox <johnmrox@gmail.com>
2017-11-26 12:08:31 -0600

    forms container component section

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -4,6 +4,7 @@ import { HttpModule } from '@angular/http';
 
 // containers
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
+import { PassengerViewerComponent } from './containers/passenger-viewer/passenger-viewer.component';
 
 // components
 import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
@@ -13,9 +14,16 @@ import { PassengerDetailComponent } from './components/passenger-detail/passenge
 import { PassengerDashboardService } from './passenger-dashboard.service';
 
 @NgModule({
-	declarations: [PassengerDashboardComponent, PassengerCountComponent, PassengerDetailComponent],
+	declarations: [
+		// container
+		PassengerDashboardComponent,
+		PassengerViewerComponent,
+		// components
+		PassengerCountComponent,
+		PassengerDetailComponent
+	],
 	imports: [CommonModule, HttpModule],
-	exports: [PassengerDashboardComponent],
+	exports: [PassengerViewerComponent],
 	providers: [PassengerDashboardService]
 })
 export class PassengerDashboardModule {}
